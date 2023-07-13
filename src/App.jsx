709e540d94eacea667dc4c8d7da14044986f6776
Abundance Esim <abundanceesim@gmail.com>
2023-07-13 18:58:34 -0400

    Build todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './styles.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// you can only ever return 1 element for a react component.
+// so to have multiple, create a big fragment or div and nest
+// other components inside it
+export default function App() {
+  const [newItem, setNewItem]  = useState('')
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos( (currentTodos)=> {
+      return [
+        ...currentTodos, 
+        {id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    })
+
+    setNewItem('')
+  }
+
+  function toggleTodo(id, completed){
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        // if my todo's id is the current id:
+        if (todo.id === id) {
+          //return the new todo with completed  = true
+          return { ...todo, completed }
+        }
+        // otherwise just return the todo anyways
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos( currentTodos => {
+      // return all other todos apart from the one where delete was clicked
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} className="new-item-form" action="">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          {/* each time an event is fired for the input, set its text */}
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        {/*Used short circuiting to display no todos if the todos array is empty*/}
+        {todos.length === 0 && "No Todos"}
+        {todos.map((todo) => {
+          return(
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+              {todo.title}
+            </label>
+            {/*for the delete, call a function that calls deleteTodo(), don't call it directly */}
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+          </li>
+          )
+        })}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
